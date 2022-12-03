70d6b9f1d4a57efe9d2f3cb7481b6722c0695df0
ichaiSimah <ichaisimah@gmail.com>
2022-12-03 18:41:46 +0200

    player added

---
+++ Player.cpp
@@ -0,0 +1,121 @@
+#include "Player.h"
+/**
+ *  char* m_name;
+    int m_level;
+    int m_force;
+    int m_maxHP;
+    int m_HP;
+    int m_coins;
+ *
+ */
+Player::Player(char *name, int maxHP = DEFAULT_STARTING_HP, int force = DEFAULT_STARTING_FORCE) : m_name(new char[strlen(name) + 1]),
+                                                                                                  m_level(DEFAULT_STARTING_LEVEL),
+                                                                                                  m_force(force),
+                                                                                                  m_maxHP(maxHP),
+                                                                                                  m_HP(maxHP),
+                                                                                                  m_coins(DEFAULT_STARTING_COINS)
+{
+    strcpy(m_name, name);
+    if (m_force < 0)
+    {
+        m_force = DEFAULT_STARTING_FORCE;
+    }
+    if (m_HP < 0)
+    {
+        m_HP = DEFAULT_STARTING_HP;
+        m_maxHP = DEFAULT_STARTING_HP;
+    }
+}
+
+Player::Player(const Player &other):m_name(new char[strlen(other.m_name)+1]),
+                                    m_level(other.m_level),
+                                    m_force(other.m_force),
+                                    m_maxHP(other.m_maxHP),
+                                    m_HP(other.m_HP),
+                                    m_coins(other.m_coins)
+{
+    strcpy(m_name, other.m_name);
+}
+Player &Player::operator=(const Player& other){
+    if(this == &other){
+        return *this;
+    }
+    delete[] m_name
+    m_name = new char[strlen(other.m_name)+1];
+    strcpy(m_name, other.m_name);
+    m_level = other.m_level;
+    m_force = other.m_force;
+    m_maxHP = other.m_maxHP;
+    m_HP = other.m_HP;
+    m_coins = other.m_coins;
+    return *this;
+}
+Player::~Player(){
+    delete[m_name];
+}
+void Player::printInfo() const{
+    printPlayerInfo(m_name, m_level, m_force, m_hp, m_coins);
+}
+
+void Player::levelUp()
+{
+    if(m_level < MAX_LEVEL){
+        m_level++;
+    }
+}
+
+int Player::getLevel() const
+{
+    return m_level;
+}
+
+void Player::buff(int forceToAdd)
+{
+    if (forceToAdd > 0)
+    {
+        m_force += forceToAdd;
+    }
+}
+void Player::heal(int HpToAdd){
+    if(HpToAdd > 0)
+    {
+        m_HP += HpToAdd;
+        if(m_HP > m_maxHP){
+            m_HP = m_maxHP;
+        }
+    }
+}
+
+void Player::damage(int HpToRemove){
+        if(HpToRemove > 0)
+    {
+        m_HP -= HpToRemove;
+        if(m_HP < 0){
+            m_HP = 0;
+        }
+    }
+}
+
+bool Player::isKnockedOut() const{
+    return(!(bool)m_HP);
+}
+
+void Player::addCoins(int coinsToAdd){
+    if(coinsToAdd > 0){
+        m_coins += coinsToAdd;
+    }
+}
+
+bool Player::pay(int coinsToPay){
+    if(coinsToPay < 0){ //TO CHECK
+        return true;
+    }
+    if(m_coins >= coinsToPay){
+        m_coins -= coinsToPay;
+        return true;
+    }
+    return false;
+}
+int Player::getAttackStrength() const{
+    return m_force;
+}
---
+++ Player.h
@@ -0,0 +1,91 @@
+#ifndef EX2_PLAYER_H
+#define EX2_PLAYER_H
+
+#include <string>
+#include <iostream>
+#include"utilities.h"
+
+const int DEFAULT_STARTING_HP = 100;
+const int DEFAULT_STARTING_FORCE = 5;
+const int DEFAULT_STARTING_LEVEL = 1;
+const int DEFAULT_STARTING_COINS = 100;
+const int MAX_LEVEL = 10;
+
+class Player
+{
+public:
+    /*
+     * C'tor of Player class
+     *
+     * @param name - The name of the player.
+     * @param maxHP - The maximum health points of the player.
+     * @param force - The starting force of the player.
+     * @return
+     *      A new instance of Player.
+    */
+    Player(char *name, int maxHP = 100, int force = 5);
+
+    /*
+     * copy C'tor of Player class
+     *
+     * @param other - the copied player.
+     * @return
+     *      A new instance of Card.
+    */
+    Player(const Player &other);
+
+    /*
+     * assignment operator of Player class
+     *
+     * @param other - the "copied" player.
+     * @return
+     *      Reference to the assigned player.
+    */
+    &Player operator=(const Player &other);
+
+    /*
+     * D'tor of Player class
+    */
+    ~Player();
+
+    /*
+     * Prints the player's infos in the form:
+     * name:
+     * level:
+     * force:
+     * hp:
+     * coins:
+     * 
+     * @return
+     *      void.
+    */
+    void printInfo() const;
+    /*
+     * 
+     *
+     * @param  - 
+     * @param  - 
+     * @param  - 
+     * @param  - 
+     * @return
+     *      
+    */
+    void levelUp();
+    int getLevel() const;
+    void buff(int forceToAdd);
+    void heal(int HpToAdd);
+    void damage(int HpToRemove);
+    bool isKnockedOut() const;
+    void addCoins(int coinsToAdd);
+    bool pay(int coinsToPay);
+    int getAttackStrength() const;
+
+private:
+    char *m_name;
+    int m_level;
+    int m_force;
+    int m_maxHP;
+    int m_HP;
+    int m_coins;
+}
+#endif // EX2_PLAYER_H
