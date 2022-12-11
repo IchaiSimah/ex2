b495637afe1f6f4ddc10d3ff8ad7a8aecbb79ae8
JarodCohen <jarod.cohen@campus.technion.ac.il>
2022-12-11 15:26:59 +0200

    comment added #2

--- Mtmchkin.h
+++ Mtmchkin.h
@@ -4,7 +4,7 @@
 
 #ifndef EX2_GAME_H
 #define EX2_GAME_H
-#include "Card.h" 
+#include "Card.h"
 #include <iostream>
 
 /*
@@ -12,59 +12,59 @@
  * MidGame - The game is still active and the player continues to encounter cards.
  * Win - The player reached level 10.
  * Loss - The player's HP is 0.
-*/
-enum class GameStatus{Win, Loss, MidGame};
-
-class Mtmchkin {
-public:
-
-    /*
-     * C'tor of the game:
-     *
-     * @param playerName - The name of the player.
-     * @param cardsArray - A ptr to the cards deck.
-     * @param numOfCards - Num of cards in the deck.
-     * @result
-     *      An instance of Mtmchkin
-    */
-    Mtmchkin(const char* playerName, const Card* cardsArray, int numOfCards);
-    //D'tor of the game
-    ~Mtmchkin();
-
-    /*
-     * Play the next Card - according to the instruction in the exercise document
-     *
-     * @return
-     *      void
-    */
-    void playNextCard();
-
-
-    /*
-     *  Check if the game ended:
-     *
-     *  @return
-     *          True if the game ended
-     *          False otherwise
-     */
-    bool isOver() const;
-
-
-    /*
-     *  Get the status of the game:
-     *
-     *  @return
-     *          GameStatus - the current status of the running game
-     */
-    GameStatus getGameStatus() const;
-
-
-private:
-  Player m_player; // represents the class player we created in player.h
-  Card* m_cards;  // represents a table of elements of type cards we created in card.h
-  int m_numOfCards; // numOfCards represents the number of elements of our table m_cards
-  int m_actualCard = 0; // actualCard keeps count of what card was previously played and which card of our table will be played next
+ */
+enum class GameStatus
+{
+  Win,
+  Loss,
+  MidGame
 };
 
+class Mtmchkin
+{
+public:
+  /*
+   * C'tor of the game:
+   *
+   * @param playerName - The name of the player.
+   * @param cardsArray - A ptr to the cards deck.
+   * @param numOfCards - Num of cards in the deck.
+   * @result
+   *      An instance of Mtmchkin
+   */
+  Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCards);
+  // D'tor of the game
+  ~Mtmchkin();
 
-#endif //EX2_GAME_H
+  /*
+   * Play the next Card - according to the instruction in the exercise document
+   *
+   * @return
+   *      void
+   */
+  void playNextCard();
+
+  /*
+   *  Check if the game ended:
+   *
+   *  @return
+   *          True if the game ended
+   *          False otherwise
+   */
+  bool isOver() const;
+
+  /*
+   *  Get the status of the game:
+   *
+   *  @return
+   *          GameStatus - the current status of the running game
+   */
+  GameStatus getGameStatus() const;
+
+private:
+  Player m_player;
+  Card *m_cards;
+  int m_numOfCards;
+  int m_actualCard = 0;
+};
+#endif // EX2_GAME_H
--- Player.h
+++ Player.h
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <iostream>
-#include"utilities.h"
+#include "utilities.h"
 
 const int DEFAULT_STARTING_HP = 100;
 const int DEFAULT_STARTING_FORCE = 5;
@@ -22,7 +22,7 @@ public:
      * @param force - The starting force of the player.
      * @return
      *      A new instance of Player.
-    */
+     */
     Player(const char *name, int maxHP = 100, int force = 5);
 
     /*
@@ -31,7 +31,7 @@ public:
      * @param other - the copied player.
      * @return
      *      A new instance of Card.
-    */
+     */
     Player(const Player &other);
 
     /*
@@ -40,12 +40,12 @@ public:
      * @param other - the "copied" player.
      * @return
      *      Reference to the assigned player.
-    */
-    Player& operator=(const Player &other);
+     */
+    Player &operator=(const Player &other);
 
     /*
      * D'tor of Player class
-    */
+     */
     ~Player();
 
     /*
@@ -55,29 +55,98 @@ public:
      * force:
      * hp:
      * coins:
-     * 
+     *
      * @return
      *      void.
-    */
+     */
     void printInfo() const;
+
     /*
-     * 
+     * Add one to the player's level.
      *
-     * @param  - 
-     * @param  - 
-     * @param  - 
-     * @param  - 
      * @return
-     *      
-    */
+     *      void
+     *
+     */
     void levelUp();
+
+    /*
+     * getter of the player's level.
+     *
+     * @return
+     *      the player's level.
+     *
+     */
     int getLevel() const;
+
+    /*
+     * Add force to the player.
+     *
+     * @param forceToAdd - the force to add to the player.
+     * @return
+     *      void
+     *
+     */
     void buff(int forceToAdd);
+
+    /*
+     * Add health Point to the player.
+     *
+     * @param HpToAdd - the health point to add to the player.
+     * @return
+     *      void
+     *
+     */
     void heal(int HpToAdd);
+
+    /*
+     * reduce health Point of the player.
+     *
+     * @param HpToRemove - the health point to remove from the player.
+     * @return
+     *      void
+     *
+     */
     void damage(int HpToRemove);
+
+    /*
+     * check if the player is knocked out.
+     *
+     * @return
+     *      true - if the health point of the player is equal zero.
+     *      false - if the health point of the player is bigger than zero
+     *
+     */
     bool isKnockedOut() const;
+
+    /*
+     * Add coins to the player.
+     *
+     * @param coinsToAdd - the coins to add to the player.
+     * @return
+     *      void
+     *
+     */
     void addCoins(int coinsToAdd);
+
+    /*
+     * check if the player can pay and make him pay.
+     *
+     * @param coinsToPay - the coins to pay.
+     * @return
+     *      true - if the coins he possess are sufficient to pay.
+     *      false - if the coins he possesses are not enough.
+     *
+     */
     bool pay(int coinsToPay);
+
+    /*
+     * getter of player's strenght.
+     *
+     * @return
+     *      'the player's strenght
+     *
+     */
     int getAttackStrength() const;
 
 private:
